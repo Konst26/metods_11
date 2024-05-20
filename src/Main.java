bb9a1358ecf58eb3bdfbd818fab0ec14d5f4c168
Константин <osokinkonstantin@mail.ru>
2024-05-20 21:00:13 +0300

    исправлены недочеты

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,16 @@ public class Main {
         checkYear(2020);
 
         printDeviceVersion(1, 2020);
-        printDeviceVersion(0, 2023);
+        printDeviceVersion(0, 2030);
         printDeviceVersion(1, 2024);
 
-        int days = calculateDistance(55);
-        System.out.println("Количество дней " + days);
+        int days = calculateDistance(111);
+        if (days > 0) {
+            System.out.println("Количество дней " + days);
+        } else {
+            System.out.println("Доставки нет");
+        }
     }
-
-
     public static void checkYear(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год - високосный год");
@@ -29,17 +31,23 @@ public class Main {
         int currentYear = LocalDate.now().getYear();
         var type = osType == 1 ? "Android" : "iOs";
         var appType = year < currentYear ? "облегченную " : "";
-        System.out.println("Установите " + appType + "версию для системы " + type);
-    }
 
-    public static int calculateDistance (int distance) {
+        System.out.println("Установите " + appType + "версию для системы " + type);
+        if (year > currentYear) {
+            System.out.println("Ошибка");
+        }
+    }
+    public static int calculateDistance(int distance) {
+        if (distance > 100) {
+            return -1;
+        }
         int days = 1;
-        if(distance > 20) {
+        if (distance > 20) {
             days++;
         }
-        if(distance > 60) {
+        if (distance > 60) {
             days++;
         }
         return days;
-        }
     }
+}
